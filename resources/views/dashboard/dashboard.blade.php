913e91aa6f3ad9f2ba8b603f8e0e4abefa3697a8
Andrea Iglio <andrea.iglio@gmail.com>
2021-03-13 15:02:17 +0100

    Fix /dashboard route and its DashboardController

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -3,21 +3,27 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        @role('admin')
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
+        @endrole
+        @role('doctor')
+        <p>diocane la madonna 8 ore per capire na cazzata</p>
+        @endrole
+
     </div>
 </div>
 @endsection
--- resources/views/doctor/dashboard.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-    <h1>DIOTIPREGOFUNZIONA</h1>
-
-@endsection
