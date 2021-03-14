47a9d0469ab84a4ba1f3ec439094b5ed413daf91
LuigiTroiano <troiano.luigi8@gmail.com>
2021-03-14 11:19:08 +0100

    Add migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,21 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('address')->nullable();
+            $table->integer('register_number_doc',10)->nullable();
+            $table->string('cv_img')->nullable();
+            $table->string('profile_img')->nullable();
+            $table->string('phone_number')->nullable();
+
+            $table->unsignedBigInteger('prefix_id')->nullable();
+            $table->foreign('prefix_id')->references('id')->on('prefixes');
+
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
