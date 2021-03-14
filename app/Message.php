911ada283a298405c50c4ae14f0ece423a2e7c15
LuigiTroiano <troiano.luigi8@gmail.com>
2021-03-14 20:27:59 +0100

    Add relations to model Message and Review

--- app/Message.php
+++ app/Message.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     protected $fillable = ['title', 'body', 'name', 'lastname', 'email', 'tax_code', 'date', 'slug'];
+    
+    public function users() {
+    
+        return $this->belongsTo('App\User');
+    }
 }
--- app/Review.php
+++ app/Review.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     protected $fillable = ['vote', 'name', 'lastname', 'title', 'body', 'date'];
+
+    public function users() {
+        return $this->belongsTo('App\User');
+    }
 }
