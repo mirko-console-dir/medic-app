3ae3eefc00eb203c4ae07bd220dbbe48f3e46377
LuigiTroiano <troiano.luigi8@gmail.com>
2021-03-14 20:19:55 +0100

    Add some seeders and relations model

--- config/seeder/services.php
+++ config/seeder/services.php
@@ -2,5 +2,19 @@
 
 
 return [
-    [],
+    [
+        'name' => 'First check-up',
+    ],
+    [
+        'name' => 'Cardiology check-up',
+    ],
+    [
+        'name' => 'Radiology check-up',
+    ],
+    [
+        'name' => 'Dermatology check-up',
+    ],
+    [
+        'name' => 'Allergies check-up',
+    ],
 ];
