fe3acdef244d8fc83a886fbcde88858cb8bda672
Andrea Iglio <andrea.iglio@gmail.com>
2021-03-14 12:35:41 +0100

    fix migrations tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,14 +22,10 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->string('address')->nullable();
-            $table->integer('register_number_doc',10)->nullable();
+            $table->integer('register_number_doc')->nullable();
             $table->string('cv_img')->nullable();
             $table->string('profile_img')->nullable();
             $table->string('phone_number')->nullable();
-
-            $table->unsignedBigInteger('prefix_id')->nullable();
-            $table->foreign('prefix_id')->references('id')->on('prefixes');
-
             $table->string('slug')->unique();
             $table->timestamps();
         });
---
+++ database/migrations/2021_03_14_112638_add_foreign_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('prefix_id')->nullable();
+            $table->foreign('prefix_id')->references('id')->on('prefixes');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            
+            $table->dropForeign('users_prefix_id_foreign');
+            $table->dropColumn('prefix_id');
+
+        });
+    }
+}
