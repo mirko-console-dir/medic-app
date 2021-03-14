c6db88a4201e74fd8b1f2e48567ae0875dd7cf65
Andrea Iglio <andrea.iglio@gmail.com>
2021-03-14 20:15:05 +0100

    Implement Review and Message model with fillable

--- app/Message.php
+++ app/Message.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    protected $fillable = ['title', 'body', 'name', 'lastname', 'email', 'tax_code', 'date', 'slug'];
 }
