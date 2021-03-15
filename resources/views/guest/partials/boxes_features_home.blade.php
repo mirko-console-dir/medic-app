c7e8495c625b5a96bebd463052a70085fe9b5aad
Tommaso Figlia <tommasofiglia@gmail.com>
2021-03-15 12:32:44 +0100

    Create structure of first main section

---
+++ resources/views/guest/partials/boxes_features_home.blade.php
@@ -0,0 +1,30 @@
+<section id="boxes_features_home">
+
+  <div class="first_row position_relative d_flex">
+
+    <div class="box intro_description">
+
+    </div>
+
+    <div class="box site_services position_absolute">
+
+    </div>
+
+  </div>
+
+  <div class="second_row position_relative">
+
+    <div class="box call_to_action position_absolute">
+
+      <div class="card position_absolute">
+
+      </div>
+    </div>
+
+    <div class="box empty position_absolute">
+
+    </div>
+  </div>
+
+
+</section>
