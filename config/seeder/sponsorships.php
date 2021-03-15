f9f514770359eb21b99380ae4159fe84bdc24b9c
LuigiTroiano <troiano.luigi8@gmail.com>
2021-03-15 09:58:04 +0100

    Modify error in sponsorship config

--- config/seeder/sponsorships.php
+++ config/seeder/sponsorships.php
@@ -10,19 +10,19 @@ return [
   [
     'name' => 'standard',
     'type' => 1,
-    'price' => 2,99,
+    'price' => 2.99,
     'duration' => 24,
   ],
   [
     'name' => 'premium',
     'type' => 2,
-    'price' => 5,99,
+    'price' => 5.99,
     'duration' => 72,
   ],
   [
     'name' => 'exclusive',
     'type' => 3,
-    'price' => 9,99,
+    'price' => 9.99,
     'duration' => 144,
   ],
 ];
