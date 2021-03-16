54473206d7cc1179cdce178321eadb2033a08c86
Tommaso Figlia <tommasofiglia@gmail.com>
2021-03-16 15:55:15 +0100

    Mediaqueries for navbar and jumbo desktop versions

--- resources/views/guest/partials/jumbo_home.blade.php
+++ resources/views/guest/partials/jumbo_home.blade.php
@@ -4,7 +4,12 @@
     @include('layouts.guest.partials.navbar')
 
     <div class="container_searchbar">
-      <form class="col-lg-6 d_flex" action="index.html" method="post">
+
+      <div class="col-lg-6 col-sm-12 text_jumbo">
+        <h2>The best place to find doctors you need.</h2>
+      </div>
+      
+      <form class="col-lg-6 col-sm-12 d_flex" action="index.html" method="post">
         <input type="text" name="search" placeholder="Search for a doctor here">
         <button type="submit" name="button">GO</button>
       </form>
