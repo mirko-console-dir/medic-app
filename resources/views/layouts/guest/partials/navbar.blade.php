3f065610fc93efe7b3d0f19d8e5688589dfe488b
Tommaso Figlia <tommasofiglia@gmail.com>
2021-03-16 02:04:14 +0100

    Graphic changes to navbar and jumbo in homepage

--- resources/views/layouts/guest/partials/navbar.blade.php
+++ resources/views/layouts/guest/partials/navbar.blade.php
@@ -2,23 +2,55 @@
   <div class="row">
 
     <div class="col-lg-3">
-      <a href="#" class="d_flex_center">
-        <img src="{{asset('img/logo.png')}}" alt="logo brutto" id="logo">
+      {{-- INSERIRE {{route('nomehome')}} --}}
+      <a href="/" class="d_flex" id="logo">
+        <img src="{{asset('img/logo.png')}}" alt="MedicUs logo">
+        <h1 class="title">MedicUs</h1>
       </a>
     </div>
 
     <div class="col-lg-6">
-      <ul class="d_flex links">
-        <li><a href="#">prova</a></li>
-        <li><a href="#">prova</a></li>
-        <li><a href="#">prova</a></li>
+
+      <ul class="links">
+        <li class="link">
+
+          {{-- INSERIRE {{route('nomehome')}} --}}
+          <a href="/">
+            <i class="fas fa-caret-right"></i>
+            Home
+            <div class="hr">
+              <hr>
+            </div>
+          </a>
+        </li>
+
+        <li class="link">
+          <a href="#">
+            <i class="fas fa-caret-right"></i>
+            Search
+            <div class="hr">
+              <hr>
+            </div>
+          </a>
+        </li>
+
+        <li class="link">
+          <a href="#">
+            <i class="fas fa-caret-right"></i>
+            link
+            <div class="hr">
+              <hr>
+            </div>
+          </a>
+        </li>
+
       </ul>
     </div>
 
     <div class="col-lg-3 d_flex buttons">
       @if (Route::has('login'))
         @auth
-          <a href="{{ url('/dashboard') }}" class="button">My Profile</a>
+          <a href="{{ url('/dashboard') }}" class="btn_blue">My Profile</a>
         @else
           <a href="{{ route('login') }}" class="btn_blue">Login</a>
 
