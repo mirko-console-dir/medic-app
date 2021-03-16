49c33b851d056462a4c783667bf1d2a618964811
Romina Trazzi <romina.trazzi@posta.it>
2021-03-16 02:46:20 +0100

    Implement footer on guest's homepage

--- resources/views/guest/partials/footer.blade.php
+++ resources/views/guest/partials/footer.blade.php
@@ -1,3 +1,88 @@
 <footer>
-  ciao
+
+  <div class="container_80">
+    
+    <!-- First footer row -->
+    <div class="row first_row">
+      
+      <div class="col-lg-4">
+        <div class="details_footer d_flex_center">
+          <p class="title_footer"> Vestibulum dapibus <br> mauris nec malesuada <br> fames ac tupis velit </p>
+          <a href=""class="buttons btn_blue">View more details</a>
+        </div>
+      </div>
+
+      <div class="col-lg-3">
+        <div class="links_footer">
+          <p class="title_footer"> Nullam wisi ultricies a <br> gravida vitae </p> 
+          
+          <div class="d_flex">
+            <div class="col-lg-6">
+              <a href=""> <i class="fas fa-chevron-right"></i> Aenean solli </a>  
+              <a href=""> <i class="fas fa-chevron-right"></i> Mauris in erat </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Nam nec tellus </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Class aplent </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Duis sed odio sit </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Morbi accusam </a> 
+            </div>
+            
+            <div class="col-lg-6">
+              <a href=""> <i class="fas fa-chevron-right"></i> Mauris in erat </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Nam nec tellus </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Class aplent </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Duis sed odio sit </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Morbi accusam </a> 
+            </div>
+          </div>
+
+        </div>
+      
+      </div>
+
+      <div class="col-lg-5">
+        <div class="logo_footer d_flex_center">
+          <img src="{{asset('img/logo.png')}}" alt="logo brutto" id="logo">
+
+        </div>
+
+
+      </div>
+    
+    </div>
+    <!-- End First footer row -->
+
+    <!-- Second footer row -->
+    <div class="row second_row">
+      <div class="col-lg-4 copyright d_flex_center">
+        <small> Copyright 2021 Medical Clinic - Team 5 <br> All right reserved</small>
+      </div>
+
+      <div class="col-lg-3">
+      </div>
+
+      <div class="col-lg-5">
+        <div class="social d_flex_center">
+          <i class="fab fa-facebook-square"></i>
+          <i class="fab fa-twitter-square"></i>
+          <i class="fab fa-instagram"></i>
+          <i class="fab fa-youtube"></i>
+          <i class="fab fa-linkedin"></i>
+        </div>
+      </div>
+    </div>
+    <!-- End Second footer row -->
+    
+
+  </div>
+
+
+
+
+
+
+
+
+
+  
+ 
 </footer>
