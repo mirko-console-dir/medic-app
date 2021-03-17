e0f2073c1968b822e389389421e1effdae718b60
Andrea Iglio <andrea.iglio@gmail.com>
2021-03-17 13:26:29 +0100

    Add services table as 1toMany with user and start the crud

--- database/migrations/2021_03_17_113518_create_services_table.php
+++ database/migrations/2021_03_17_113518_create_services_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserServiceTable extends Migration
+class CreateServicesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,15 @@ class CreateUserServiceTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_service', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id');
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->float('price')->nullable();
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users');
-            $table->unsignedBigInteger('service_id');
-            $table->foreign('service_id')->references('id')->on('services');
+
         });
     }
 
@@ -28,6 +32,6 @@ class CreateUserServiceTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_service');
+        Schema::dropIfExists('services');
     }
 }
---
+++ resources/views/dashboard/doctor/service/create.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+
+<div class="container d_flex" id="dashboard">
+
+    @role('doctor')
+    @include('dashboard.partials.sidebar')
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div><br />
+    @endif
+    <div class="d_flex_column detail" id="account">
+        <div class="my_profile d_flex_column">
+            <div class="detail d_flex_column">
+                <h2>Create new service</h2>
+                <form method="post" action="{{ route('dashboard.services.store') }}" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group d_flex_column">
+
+                        <!-- Name -->
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name" class="form-control" placeholder="Insert your name" value="" required>
+                        <br>
+
+
+                        <!-- address -->
+                        <label for="description">Description</label>
+                        <input type="text" name="description" id="description" class="form-control" placeholder="Insert your description" value="" >
+                        <br>
+
+
+                        <!-- price -->
+                        <label for="price">Price</label>
+                        <input type="number" name="price" id="price" class="form-control" placeholder="Insert your price" value="" step="0.01">
+                        <br>
+
+
+                        <button type="submit" class="btn btn-success">Create</button>
+
+
+                        </form>
+
+
+                    </div>
+
+            </div>
+
+        </div>
+
+
+
+    </div>
+
+
+
+    @endrole
+</div>
+@endsection
