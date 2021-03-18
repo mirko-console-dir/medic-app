7ef061be219e93426549323da1f20259cadf0757
andreamazza84 <andrea.mazza84@gmail.com>
2021-03-18 15:52:22 +0100

    round-carosello

--- resources/js/components/slideshow.vue
+++ resources/js/components/slideshow.vue
@@ -1,14 +1,15 @@
+<!-- nuovo -->
 <template>
     <div id="slideshow">
         <i class="fa fa-chevron-left" @click="prev()"></i>
         <div class="card_container">
             <transition-group id="card-complete" name="card-complete" tag="div">
-                <div class="card-complete-item"  :style="{'width': cardWidth}" v-for="profile in profiles.slice(i,i+show)" :key="profile.id">
+                <div class="card-complete-item"  :style="{'width': cardWidth}" v-for="profile in activeProfiles" :key="profile.id">
                     <div class="info avatar" :style="{ 'background-image': 'url('+image.path+profile.id+image.ext+')' }"></div>
                     <h4 class="info name">{{profile.sex === 'm'?'Dott.':'Dott.ssa'}} {{profile.name}} {{profile.lastname}}</h4>
                     <h4 class="info specialization">{{profile.specialization}}</h4>
                     <p class="info presentation">{{profile.presentation}}</p>
-                    <!-- <a class="info presentation" href="/dashboard/doctors/{doctor}">link</a> -->
+                    <a class="info presentation" href="/dashboard/doctors/{doctor}">ID: {{profile.id}}</a>
                 </div>
             </transition-group>
         </div>
@@ -17,12 +18,14 @@
 </template>
 <script>
 
+/** Il carosello così impostato può visualizzare fino a un massimo di 4 card.
+*
+*/
 module.exports = {
     props: ['homeRoute'],
     data: function () {
         return {
-            
-            show: 4,
+            show: 0,
             size: {
                 sm: 1,
                 md: 2,
@@ -30,7 +33,10 @@ module.exports = {
                 xl: 4,
             },              // card mostrate contemporaneamente
             cardWidth: 1,   // larghezza card in percentuale 
-            i: 0,           // puntatore nell'array profiles
+            i: 0,
+            j: 0,
+            k: 0,
+            l: 0,           // puntatore nell'array profiles
             window: {       // dichiarazione iniziale per la variabile window
                 width: 0,
                 height: 0,
@@ -39,6 +45,7 @@ module.exports = {
                 path: 'img/sponsored/profile_',
                 ext: '.jpg'
                 },
+            activeProfiles: [],
             profiles: [
                 {
                 id: '01',
@@ -136,68 +143,152 @@ module.exports = {
                 sex: 'f',
                 presentation: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente blanditiis consectetur soluta magni ab officiis assumenda odit cum voluptate fuga, omnis ea laboriosam adipisci tempore?!'
                 },
-
-                
             ],
         }
     },
    
     methods: {
-        prev: function() {
+        prev: function(initial) {
             let i = this.i;
-            i--;
-            if(i < 0){
-                return this.i = this.profiles.length - this.show;
-            }
-            return this.i = i;
+            let j = this.j;
+            let k = this.k;
+            let l = this.l;
+            let show = this.show;
+            let activeProfiles = this.profiles;
+            let profilesLength = this.profiles.length;
             
-        },
-        next: function() {
-            let i = this.i;
-            i++;
-            if(i > this.profiles.length - this.show){
-                return this.i = 0;
+            if(!initial){
+                i--; j--; k--; l--;
             }
-            return this.i = i;
+            
+            if(i < 0){
+                i = profilesLength - 1;
+            }
+            if(j < 0){
+                j = profilesLength - 1;
+            }
+            if(k < 0){
+                k = profilesLength - 1;
+            }
+            if(l < 0){
+                l = profilesLength - 1;
+            }
+
+            if(show === 1){
+                activeProfiles = [activeProfiles[i]];
+            }
+            else if(show === 2){
+                activeProfiles = [activeProfiles[i], activeProfiles[j]];
+            }
+            else if(show === 3){
+                activeProfiles = [activeProfiles[i], activeProfiles[j], activeProfiles[k]];
+            }
+            else if(show === 4){
+                activeProfiles = [activeProfiles[i], activeProfiles[j], activeProfiles[k], activeProfiles[l]];
+            }
+
+            // console.log("l = ", l);
+            // console.log("k = ", k);
+            // console.log("j = ", j);
+            // console.log("i = ", i);
+            
+            this.i = i;
+            this.j = j;
+            this.k = k;
+            this.l = l;
+            
+            return this.activeProfiles = activeProfiles;
+        },
+
+        next: function(initial) {
+            let i = this.i;
+            let j = this.j;
+            let k = this.k;
+            let l = this.l;
+            let show = this.show;
+            let activeProfiles = this.profiles;
+            let profilesLength = this.profiles.length;
+            
+            if(!initial){
+                i++; j++; k++; l++;
+            }
+            if(l > profilesLength - 1){
+                l = 0;
+            }
+            if(k > profilesLength - 1){
+                k = 0;
+            }
+            if(j > profilesLength - 1){
+                j = 0;
+            }
+            if(i > profilesLength - 1){
+                i = 0;
+            }
+
+            if(show === 1){
+                activeProfiles = [activeProfiles[i]];
+            }
+            else if(show === 2){
+                activeProfiles = [activeProfiles[i], activeProfiles[j]];
+            }
+            else if(show === 3){
+                activeProfiles = [activeProfiles[i], activeProfiles[j], activeProfiles[k]];
+            }
+            else if(show === 4){
+                activeProfiles = [activeProfiles[i], activeProfiles[j], activeProfiles[k], activeProfiles[l]];
+            }
+
+            // console.log("l = ", l);
+            // console.log("k = ", k);
+            // console.log("j = ", j);
+            // console.log("i = ", i);
+            
+            this.i = i;
+            this.j = j;
+            this.k = k;
+            this.l = l;
+
+            return this.activeProfiles = activeProfiles;
         },
         /** Shows the window width and height 
         *   
         */
-        handleResize: function() {
+        cardMediaQuery: function() {
             this.window.width = window.innerWidth;
             this.window.height = window.innerHeight;
-        },
-        cardMediaQuery: function() {
+            //tablet
             if(window.innerWidth < 991.98 && window.innerWidth > 768) {
+                this.next(true);
                 this.show = this.size.md;
             }
-            else if(window.innerWidth  > 992 && window.innerWidth  < 1199.98){
-                this.show = this.size.lg;
-            }
+            //mobile
             else if(window.innerWidth  < 767.98){
+                this.next(true);
                 this.show = this.size.sm;
             }
-            else if(window.innerWidth  > 1200){
-                this.show = this.size.xl;
+            //desktop
+            else if(window.innerWidth  > 992){
+                this.next(true);
+                this.show = this.size.lg;
             }
-            this.cardWidth = (1/this.show);
+            return this.cardWidth = (1/this.show);
         }
-
     },
     created() {
-        window.addEventListener('resize', this.handleResize);
         window.addEventListener('resize', this.cardMediaQuery);
-        this.handleResize();
         this.cardMediaQuery();
-
     },
     mounted() {
-        console.log('Component Slideshow');
+        console.log('Component "Slideshow" mounted');
+        this.i = 0;
+        this.j = this.i + 1;
+        this.k = this.i + 2;
+        this.l = this.i + 3;
+        this.next(true);
     },
-
     distroyed() {
-        window.removeEventListener('resize', this.handleResize);
         window.removeEventListener('resize', this.cardMediaQuery);
     },
 }
 </script>
+
