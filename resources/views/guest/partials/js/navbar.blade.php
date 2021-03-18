76b4e64d3590122306c82c22ca8629469ffebc6e
Tommaso Figlia <tommasofiglia@gmail.com>
2021-03-18 03:31:30 +0100

    recreate navbar animations on desktop size

--- resources/views/guest/partials/js/navbar.blade.php
+++ resources/views/guest/partials/js/navbar.blade.php
@@ -1,6 +1,15 @@
 <script type="application/javascript">
 
+// Animazione sulla navbar a seconda dello scroll verticale
+window.addEventListener('scroll', (event) => {
+  var scroll = this.scrollY;
 
+  if (scroll >= 380) {
+    document.getElementById('navbar').classList.add('fixed_top');
+  } else {
+    document.getElementById('navbar').classList.remove('fixed_top');
+  }
+})
 
 
 </script>
