29d0ae0be4477c93163dbafcdefdf79172988b60
Romina Trazzi <romina.trazzi@posta.it>
2021-03-20 20:14:18 +0100

    Finish footer mediaquery

--- resources/views/guest/partials/footer.blade.php
+++ resources/views/guest/partials/footer.blade.php
@@ -18,16 +18,17 @@
           <p class="title_footer"> Nullam wisi ultricies a <br> gravida vitae </p> 
           
           <div class="d_flex adjustment">
-
-            <div class="col-lg-6 col-sm-6 visibility_double">
+            
+            <div class="col-lg-6 col-md-12 col-sm-6 col-xs-6 d_flex"> 
               <a href=""> <i class="fas fa-chevron-right"></i> Aenean solli </a>  
               <a href=""> <i class="fas fa-chevron-right"></i> Mauris in erat </a> 
               <a href=""> <i class="fas fa-chevron-right"></i> Nam nec tellus </a> 
               <a href=""> <i class="fas fa-chevron-right"></i> Class aplent </a> 
               <a href=""> <i class="fas fa-chevron-right"></i> Duis sed odio sit </a> 
               <a href=""> <i class="fas fa-chevron-right"></i> Morbi accusam </a>
-            </div> 
-            <div class="col-lg-6 col-sm-6 visibility_double">
+            </div>
+             
+            <div class="col-lg-6 col-md-12 col-sm-6 col-xs-6 d_flex"> 
               <a href=""> <i class="fas fa-chevron-right"></i> Mauris in erat </a> 
               <a href=""> <i class="fas fa-chevron-right"></i> Nam nec tellus </a> 
               <a href=""> <i class="fas fa-chevron-right"></i> Class aplent </a> 
@@ -35,24 +36,12 @@
               <a href=""> <i class="fas fa-chevron-right"></i> Morbi accusam </a> 
             </div>
 
-            <div class="col-md-12 col-xs-12 visibility_single">
-              <a href=""> <i class="fas fa-chevron-right"></i> Aenean solli </a>  
-              <a href=""> <i class="fas fa-chevron-right"></i> Mauris in erat </a> 
-              <a href=""> <i class="fas fa-chevron-right"></i> Nam nec tellus </a> 
-              <a href=""> <i class="fas fa-chevron-right"></i> Class aplent </a> 
-              <a href=""> <i class="fas fa-chevron-right"></i> Duis sed odio sit </a> 
-              <a href=""> <i class="fas fa-chevron-right"></i> Morbi accusam </a> 
-              <a href=""> <i class="fas fa-chevron-right"></i> Mauris in erat </a> 
-              <a href=""> <i class="fas fa-chevron-right"></i> Nam nec tellus </a> 
-              <a href=""> <i class="fas fa-chevron-right"></i> Class aplent </a> 
-              <a href=""> <i class="fas fa-chevron-right"></i> Duis sed odio sit </a> 
-              <a href=""> <i class="fas fa-chevron-right"></i> Morbi accusam </a> 
-            </div>
           </div>
 
         </div>
       
       </div>
+     
      
       <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12 details">
         <div class="details_footer d_flex_center">
@@ -93,4 +82,58 @@
 </footer>
 
 
-<!-- col-md-8 col-sm-8 col-xs-8 -->
+
+
+
+<!-- 
+
+            <div class="col-lg-6 col-sm-6 visibility_double">
+              <a href=""> <i class="fas fa-chevron-right"></i> Aenean solli </a>  
+              <a href=""> <i class="fas fa-chevron-right"></i> Mauris in erat </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Nam nec tellus </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Class aplent </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Duis sed odio sit </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Morbi accusam </a>
+            </div> 
+            <div class="col-lg-6 col-sm-6 visibility_double">
+              <a href=""> <i class="fas fa-chevron-right"></i> Mauris in erat </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Nam nec tellus </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Class aplent </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Duis sed odio sit </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Morbi accusam </a> 
+            </div>
+
+            <div class="col-md-12 col-xs-12 visibility_single">
+              <a href=""> <i class="fas fa-chevron-right"></i> Aenean solli </a>  
+              <a href=""> <i class="fas fa-chevron-right"></i> Mauris in erat </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Nam nec tellus </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Class aplent </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Duis sed odio sit </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Morbi accusam </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Mauris in erat </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Nam nec tellus </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Class aplent </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Duis sed odio sit </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Morbi accusam </a> 
+            </div>
+
+            -->
+
+            <!-- <div class="col-lg-12 md-12 col-xs-12">
+              <a href=""> <i class="fas fa-chevron-right"></i> Aenean solli </a>  
+              <a href=""> <i class="fas fa-chevron-right"></i> Mauris in erat </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Nam nec tellus </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Class aplent </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Duis sed odio sit </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Morbi accusam </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Mauris in erat </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Nam nec tellus </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Class aplent </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Duis sed odio sit </a> 
+              <a href=""> <i class="fas fa-chevron-right"></i> Morbi accusam </a> 
+            </div>  -->
+
+
+           
+
+            
