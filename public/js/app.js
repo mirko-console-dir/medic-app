7d7b20b1eca2211449563c11bdb33387be0b742a
Tommaso Figlia <tommasofiglia@gmail.com>
2021-03-24 22:38:32 +0100

    Add some style on ul into search bars | correct list's dimension problem

--- public/js/app.js
+++ public/js/app.js
@@ -38958,7 +38958,7 @@ var render = function() {
             attrs: {
               type: "text",
               name: "",
-              placeholder: "start typing a specialization"
+              placeholder: "Start typing a specialization"
             },
             domProps: { value: _vm.search },
             on: {
@@ -39128,7 +39128,7 @@ var render = function() {
           attrs: {
             type: "text",
             name: "search",
-            placeholder: "start typing a specialization"
+            placeholder: "Start typing a specialization"
           },
           domProps: { value: _vm.search },
           on: {
@@ -51941,13 +51941,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
-__webpack_require__(/*! D:\Andrea\C0d1ng\Boolean.Career\Esercizi\Esercitazioni\medicUs\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Andrea\C0d1ng\Boolean.Career\Esercizi\Esercitazioni\medicUs\resources\sass\app.scss */"./resources/sass/app.scss");
-=======
-__webpack_require__(/*! C:\Users\win7\Google Drive\Boolean\ProgettoFinale\medicUs\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\win7\Google Drive\Boolean\ProgettoFinale\medicUs\resources\sass\app.scss */"./resources/sass/app.scss");
->>>>>>> AM-Frontend-20210324
+__webpack_require__(/*! C:\Users\tomma\Desktop\Boolean\PROGETTO FINALE REPO\medicUs\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\tomma\Desktop\Boolean\PROGETTO FINALE REPO\medicUs\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
