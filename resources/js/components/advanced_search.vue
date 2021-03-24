b817c2d2194a05471070c40cb222d0f5bdffc5bc
andreamazza84 <andrea.mazza84@gmail.com>
2021-03-24 17:13:24 +0100

    work on display doctors

--- resources/js/components/advanced_search.vue
+++ resources/js/components/advanced_search.vue
@@ -1,27 +1,44 @@
 <template>
-  <form  class="col-lg-6 col-md-12"  autocomplete="off">
 
-      <div class=" input_container no_blur">
-        <input class="form-control text-capitalize no_blur" type="text" name="" placeholder="Search for Specializations"
-          v-model="search"
-          @keyup="specFilter(search)"
-          @click="showList()"
-        >
+  <div class="component">
+    <form  class="col-lg-6 col-md-12"  autocomplete="off">
+
+        <div class=" input_container no_blur">
+          <input class="form-control text-capitalize no_blur" type="text" name="" placeholder="start typing a specialization"
+            v-model="search"
+            @keyup="specFilter(search)"
+            @click="showList()"
+          >
+        </div>
+
+        <ul id="spec_list" v-if="search.length === 0" :class="show?'active':''" >
+          <li v-for="spec in specializations" >
+            <a class="no_blur" href="#" @click="writeSpec(spec)">{{spec}}</a>
+          </li>
+        </ul>
+
+        <ul id="spec_list" v-else :class="show?'active':''" >
+          <li v-for="spec in filterSpec">
+            <a class="no_blur" href="#" @click="writeSpec(spec)">{{spec}}</a>
+          </li>
+        </ul>
+
+    </form>
+
+    <div class="doctors_show">
+      <div class="card_container d_flex">
+        <div class="card_wrapper">
+          <a href="#" class="card">
+            <div class="avatar"></div>
+            <h4 class="name">Dott.ssa Cippa lippa</h4>
+            <h4 class="specialization">Neurologia</h4>
+            <p class="description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod reiciendis nulla fuga corporis, dolorem dignissimos quae quisquam aperiam sint nisi.</p>
+            <div class="rating">*****</div>
+          </a>
+        </div>
       </div>
-
-      <ul id="spec_list" v-if="search.length === 0" :class="show?'active':''" >
-        <li v-for="spec in specializations" >
-          <a class="no_blur" href="#" @click="writeSpec(spec)">{{spec}}</a>
-        </li>
-      </ul>
-
-      <ul id="spec_list" v-else :class="show?'active':''" >
-        <li v-for="spec in filterSpec">
-          <a class="no_blur" href="#" @click="writeSpec(spec)">{{spec}}</a>
-        </li>
-      </ul>
-
-  </form>
+    </div>
+  </div>
 
 </template>
 
@@ -63,12 +80,14 @@
             return this.show = true;
           }
         },
+
         cookie: function() {
           return document.cookie = "search="+this.search;
         },
+
         writeSpec: function(spec){
           return this.search = spec;
-        }
+        },
 
       },
 
@@ -92,6 +111,10 @@
             console.log(error);
         });
 
+        /**
+        * Se il cookie contiene una chiave di ricerca, questa viene estratta.
+        * Alla chiusura della pagina il cookie viene cancellato.
+        */
         if(document.cookie.includes("search")){
           this.search = document.cookie.split('; ')
           .find(row => row.startsWith('search='))
@@ -104,13 +127,14 @@
         * delle specializzazioni quando si clicca su elementi senza classe 'no_blur'
         */
         document.addEventListener('click', (event)=>{
-          if(event.target.className !='no_blur'){
+          if(!event.target.className.includes('no_blur')){
             return this.show = false;
           }
         });
 
-        console.log('Component "Searchhome" mounted');
+        console.log('Component "Advanced-search" mounted');
       },
+
       destroyed(){
         document.removeEventListener('click');
       },
--- resources/views/guest/search.blade.php
+++ resources/views/guest/search.blade.php
@@ -6,21 +6,31 @@ Advanced Search
 
 <div id="advanced_search">
 
-@section('content')
-@include('layouts.guest.partials.navbar')
-<main>
-  <div class="container_80 row">
+  @section('content')
+  @include('layouts.guest.partials.navbar')
+    <main>
+      <div class="container_80 row">
+        <h1 class="title col-lg-12">Advanced Search </h1>
+        <advanced_search api="{{route('api.users')}}"></advanced_search>
+      </div>
+      {{-- card display --}}
+      {{-- <div class="container_80">
+        <div class="card_container d_flex">
+          <div class="card_wrapper">
+            <a href="#" class="card">
+              <div class="avatar"></div>
+              <h4 class="name">Dott.ssa Cippa lippa</h4>
+              <h4 class="specialization">Neurologia</h4>
+              <p class="description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod reiciendis nulla fuga corporis, dolorem dignissimos quae quisquam aperiam sint nisi.</p>
+              <div class="rating">*****</div>
+            </a>
+          </div>
+        </div>
+      </div> --}}
 
-    <h1 class="title col-lg-12">Advanced Search </h1>
-    <advanced_search api="{{route('api.users')}}"></advanced_search>
-
+    </main>
+    
   </div>
-
-
-</main>
-
-
-@include('guest.partials.footer')
-
-</div>
-@endsection
+  
+  @include('guest.partials.footer')
+  @endsection
