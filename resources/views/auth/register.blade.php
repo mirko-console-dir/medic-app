17aa793d6721fc6ef3b7bbf46112806099cf9999
Romina Trazzi <romina.trazzi@posta.it>
2021-03-25 14:37:35 +0100

    Fix Register button

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -95,7 +95,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-12 d_flex btn_register">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary btn_blue">
                                     {{ __('Register') }}
                                 </button>
                             </div>
