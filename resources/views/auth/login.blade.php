07a0e3fc8b318f7e6248ac6c701e35b28f188090
Andrea Iglio <andrea.iglio@gmail.com>
2021-03-29 11:25:20 +0200

    Fix login and reset password style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title')
-  Login
+Login
 @endsection
 
 @section('content')
@@ -11,38 +11,42 @@
             <div class="card-custom">
                 <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
+                <div class="form">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail:') }}</label>
+                        <div class="form_label_input d_flex">
 
-                            <div class="col-md-6">
+                            <div class="label d_flex">
+                                <label for="email" class="col-form-label text-md-right">{{ __('E-Mail:') }}</label>
+                            </div>
+                            <div class="input">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
+
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password:') }}</label>
+                        <div class="form_label_input d_flex">
+                            <div class="label d_flex">
+                                <label for="password" class=" col-form-label text-md-right">{{ __('Password:') }}</label>
+                            </div>
 
-                            <div class="col-md-6">
+                            <div class="input">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <div class="col-md-12">
                                 <div class="form-check d_flex">
@@ -62,9 +66,9 @@
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
                                 @endif
                             </div>
                         </div>
